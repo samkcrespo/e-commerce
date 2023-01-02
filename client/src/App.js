93d276c35b54051d84cfe9a75592fe95cbfbc37d
Samantha Crespo <samkcrespo@gmail.com>
2023-01-02 14:57:58 -0800

    create client side login, signup, signout files

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
-
+import SignupForm from "./signup/SignupForm";
+import LoginForm from "./signup/LoginForm";
+import Logout from "./signup/Logout";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {" "}
         <h1> E-Commerce Application</h1>
+        <Logout />
       </header>
-      <div></div>
+      <div>
+        <SignupForm />
+        <LoginForm />
+      </div>
     </div>
   );
 }
