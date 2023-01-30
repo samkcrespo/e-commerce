42e848941ee459ca62ee8347806cd02af695d5b5
Samantha Crespo <samkcrespo@gmail.com>
2023-01-30 14:54:16 -0800

    add /me route, update package.json file

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :carts
   resources :users
 
+  get '/me', to: 'users#show'
+
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   # Routing logic: fallback requests for React Router.
