b3333d08f495db59840be23bad6585cdeadf9837
Samantha Crespo <samkcrespo@gmail.com>
2023-02-21 15:48:01 -0800

    orders controller methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,28 @@
 class OrdersController < ApplicationController
+    
+    before_action :set_order, only: [:update]
+    skip_before_action :authorize, only: [:index]
+
+    def index 
+        orders = Order.all.order(:created_at) 
+        render json: orders, include: ['user', 'selected_items', 'selected_items.sku'], status: :ok
+    end
+
+
+    def update 
+        @order.update(order_params)
+        render json: @order, include: ['user', 'selected_items', 'selected_items.sku'], status: :accepted
+    end
+
+
+    private 
+
+    def set_order
+        @order = Order.find(params[:id])
+    end
+
+    def order_params
+        params.permit(:status)
+    end
+
 end
