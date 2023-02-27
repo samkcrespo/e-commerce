5794e04d3ddb7ac9c36b8d48c1bd3398b258e9af
Samantha Crespo <samkcrespo@gmail.com>
2023-02-27 15:00:45 -0800

    finish sku controller

--- app/controllers/skus_controller.rb
+++ app/controllers/skus_controller.rb
@@ -1,2 +1,30 @@
 class SkusController < ApplicationController
+    before_action :set_sku, only: [:update, :destroy]
+
+    def create 
+        sku = Sku.create(sku_params)
+        render json: sku, status: :created
+    end
+
+    def update 
+        @sku.update(sku_params)
+        render json: @sku, status: :accepted
+    end
+
+    def destroy 
+        @sku.destroy 
+        head :no_content
+    end
+
+
+    private
+
+    def set_sku
+        @sku = Sku.find(params[:id])
+    end
+
+    def sku_params
+        params.permit(:product_id, :size, :color, :price, :quantity, :image_key)
+    end
+
 end
