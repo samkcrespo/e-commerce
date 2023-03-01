afa688848c4dd1cea6bdf655e7d78a1a1e9b389e
Samantha Crespo <samkcrespo@gmail.com>
2023-03-01 14:04:40 -0800

    adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  resources :skus
+  resources :skus, only: [:create, :update, :destroy]
   resources :orders
   resources :product_categories
   resources :categories
   resources :products
-  resources :carts
+  resources :carts, only: [:update]
+  
   resources :users, only: [:destroy, :update]
-
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   get '/me', to: 'users#show'
-
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   # Routing logic: fallback requests for React Router.
