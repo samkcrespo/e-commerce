6006099ead7ce89e0d1d5931694818d09cf74e30
Samantha Crespo <samkcrespo@gmail.com>
2023-03-08 18:41:52 -0800

    add relationships to category and order serializers

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,3 +1,4 @@
 class CategorySerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :image_preview
+  has_many :products
 end
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
   attributes :id, :address, :status, :invoice, :email, :name
   has_one :user
+  has_many :selected_items
 end
